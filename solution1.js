bef436914b1d844fd60876aac202210490f7867d
Подивилов Евгений <Evgeniy.Podivilov@chelpipe.ru>
2015-11-20 10:44:52 +0500

    Complite task 1. Add polyfill for Function.prototype.bind.

---
+++ solution1.js
@@ -0,0 +1,29 @@
+function bind() {
+    var func, context, args;
+    if(this instanceof Function) {
+        func = this;
+        context = arguments[0];
+        args = Array.prototype.slice.call(arguments,1);
+    } else if(arguments[0] instanceof Function){
+        func = arguments[0];
+        context = arguments[1];
+        args = Array.prototype.slice.call(arguments,2);
+    }
+    return function(){
+        return func.apply(context, args.concat(Array.prototype.slice.call(arguments)));
+    }
+}
+
+Function.prototype.myBind = bind;
+
+var o = {
+    x: 1,
+    foo: function (a, b) {
+        return this.x + a + b;
+    }
+};
+var f1 = o.foo.bind({x:2}, 1);
+var f2 = bind(o.foo, {x:2}, 1);
+var f3 = bind(bind(o.foo, {x:2}), {}, 1);
+console.log(f1(5) === f2(5));
+console.log(f1(5) === f3(5));
